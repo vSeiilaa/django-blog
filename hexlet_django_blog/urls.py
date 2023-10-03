54e640557908438580d705d3d4904fef1ec4c3a6
seiilaa <vseiilaa@gmail.com>
2023-10-03 17:43:40 +0400

    add HomePageView class for views

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -17,12 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 from hexlet_django_blog import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.HomePageView.as_view()),
     path('about/', views.about),
     path('admin/', admin.site.urls),
     path('articles/', include('hexlet_django_blog.article.urls')),
