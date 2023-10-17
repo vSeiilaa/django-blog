1ee2a068ea6c2e9fd070be3eb32f2384e1d5aff8
seiilaa <vseiilaa@gmail.com>
2023-10-17 16:12:12 +0400

    add article show

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
-    path('<str:tags>/<int:article_id>', 
-        views.index, 
-        name='article'),
-        ]
+    path('', IndexView.as_view()),
+path('<int:id>/', ArticleView.as_view())
+]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+
+
+from hexlet_django_blog.article.models import Article
 
 # Create your views here.
-def index(request, tags, article_id):
+class IndexView(View):
 
-    return redirect(reverse('article', kwargs={'article_id': article_id, 'tags': tags}))
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
+    
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
